a787122adb55287dc664bd07ea43d8ade0a108fe
bon-nop <forworks.web@gmail.com>
2023-08-17 12:06:55 +0700

    first commit

---
+++ src/components/ShortUrl.vue
@@ -0,0 +1,51 @@
+<script>
+import axios from 'axios';
+import QRCode from 'qrcode-generator';
+
+const backendURL = 'http://localhost:3000';
+
+export default {
+  data() {
+    return {
+      originalUrl: '',
+      shortCode: null,
+      qrCodeImageUrl: null,
+      clickCount: null,
+    };
+  },
+  methods: {
+    async shortenUrl() {
+      try {
+        const response = await axios.post(`${backendURL}/shorten`, { originalUrl: this.originalUrl });
+        this.shortCode = response.data.shortCode;
+        this.clickCount = 0;
+
+        // Generate QR code on the client side
+        const qrCode = QRCode(0, 'H');
+        qrCode.addData(this.getShortUrl());
+        qrCode.make();
+
+        // Set the QR code image URL
+        this.qrCodeImageUrl = qrCode.createDataURL();
+      } catch (error) {
+        console.error('Error shortening URL:', error);
+      }
+    },
+    getShortUrl() {
+      return backendURL + '/' + this.shortCode;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="originalUrl" class="p-3" placeholder="Enter link here">
+    <button @click="shortenUrl">Shorten</button>
+  </div>
+
+  <div v-if="shortCode">
+    <p>Short URL: <a :href="getShortUrl()" target="_blank">{{ getShortUrl() }}</a></p>
+    <img :src="qrCodeImageUrl" alt="QR Code">
+  </div>
+</template>
